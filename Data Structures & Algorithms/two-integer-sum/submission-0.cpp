d0790038f90cb9b3831d159179044bb0c9131382
satish sahu <95178698+sahusateesh8737@users.noreply.github.com>
2026-09-05 22:45:46 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int , int>mp;
+        for(int i =0;i<nums.size();i++){
+            int remain = target - nums[i];
+            if(mp.find(remain) != mp.end()){
+                return { mp[remain] , i};
+            }
+            else {
+                mp[nums[i]] = i;
+            }
+        }
+        return {-1 , -1};
+    }
+};
